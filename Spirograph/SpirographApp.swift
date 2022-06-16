fe872287e7794911279e785f467915015f095ef0
SilverRooney <silver.rooney.dev@gmail.com>
2022-06-16 13:43:40 -0700

    Initial Commit

---
+++ Spirograph/SpirographApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpirographApp.swift
+//  Spirograph
+//
+//  Created by Silver on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SpirographApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
